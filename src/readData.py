2d314272f1b7091f46d65b78d1bbf19879a4887c
Janahan Rajagopalan <70968433+rajagopj@users.noreply.github.com>
2020-11-07 15:12:35 -0500

    readData update

--- src/readData.py
+++ src/readData.py
@@ -1,39 +1,39 @@
 from countryinfo import CountryInfo
 import math
+def readData():
+    f = open("testing.txt", "r")
 
-f = open("testing.txt", "r")
+    Country_Name =  {}
 
-Country_Name =  {}
+    size = [0, 0, 0, 0, 0, 0]
 
-size = [0, 0, 0, 0, 0, 0]
+    for line in f:
+        meas = line.split(',')
 
-for line in f:
-    meas = line.split(',')
+        for i in range(1,6):
+            meas[i] = meas[i].strip()
+            if meas[i] == "[]\n":
+                meas[i]="[0]\n"
+            elif meas[i] == "[]":
+                meas[i]="[0]"
+            print (meas[i])
+        size[0] = len(meas[0])
+        size[1] = len(meas[1])
+        size[2] = len(meas[2])
+        size[3] = len(meas[3])
+        size[4] = len(meas[4])
+        size[5] = len(meas[5])
 
-    for i in range(1,6):
-        if meas[i] == " []\n":
-            meas[i]=" [0]\n"
-        elif meas[i] == " []":
-            meas[i]=" [0]"
-            print (i)
-        print (meas[i])
-    size[0] = len(meas[0])
-    size[1] = len(meas[1])
-    size[2] = len(meas[2])
-    size[3] = len(meas[3])
-    size[4] = len(meas[4])
-    size[5] = len(meas[5])
+        country = CountryInfo( meas[0][2 : (size[0] - 2)])
+        lat_lng = country.latlng()
 
-    print(meas[0][2:(size[0]-2)])
-
-    Country_Name[meas[0][2:(size[0]-2)]] = { meas[0][2 : (size[0] - 2)]: {
-        "Total_Cases": int(meas[1][2 : (size[1] - 1)]), 
-        "New_Cases": int(meas[2][2 : (size[2] - 1)]),
-        "Total_Deaths" : int(meas[3][2 : (size[3] - 1)]),
-        "Total_Recovered" : int(meas[4][2 : (size[4] - 1)]),
-        "Active_Cases" : int(meas[5][2 : (size[5] - 2)])}
-    }
-
-f.close()
-print(Country_Name['Peru'])
-
+        Country_Name[meas[0][2:(size[0]-2)]] = { meas[0][2 : (size[0] - 2)]: {
+            "Total_Cases": int(meas[1][1 : (size[1] - 1)]), 
+            "New_Cases": int(meas[2][1 : (size[2] - 1)]),
+            "Total_Deaths" : int(meas[3][1 : (size[3] - 1)]),
+            "Total_Recovered" : int(meas[4][1 : (size[4] - 1)]),
+            "Active_Cases" : int(meas[5][1 : (size[5] - 1)]),
+            "Latitude" : lat_lng[0],
+            "Longitude" : lat_lng[1]}
+        }
+    f.close()
